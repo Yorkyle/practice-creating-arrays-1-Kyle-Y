b22bb2b9cb2f375cd46cf4d1f8ba173b1919dab1
Cybersplunker <yorkyle@gmail.com>
2025-07-17 10:40:26 -0500

    My first commit

--- practice-creating-arrays.js
+++ practice-creating-arrays.js
@@ -1,1 +1,10 @@
+//Task 1: Create Arrays
 
+let groceryArray = ["yogurt", "granola", "raisins", "raw honey", "chocolate chips"];
+let priceList = [5.49, 3.29, 1.19];
+
+console.log(groceryArray);
+console.log(priceList);
+
+console.log(`${groceryArray[0]}, ${groceryArray[1]}, ${groceryArray[3]}`);
+console.log(`${priceList[0]}, ${priceList[1]}`);
